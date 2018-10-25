f6bc9f287ad58cdfc1becaaaab5041a6f1737711
gemunulk <gemunu.lk@gmail.com>
2018-10-25 12:10:38 +0530

    Initial commit

---
+++ specs/gui_login/simple_login.spec
@@ -0,0 +1,13 @@
+Login Test Specification
+=========================
+This is an executable specification file. This file follows markdown syntax. Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+To execute this specification, use
+	npm test
+
+Existing user login
+---------------------
+tags: existing-user, login, smoke, regression
+
+* On the Login page
+* Login as "gem" using "gem"
+* Page contains "Use our Flight Finder"
---
+++ tests/dsl/login_spec.js
@@ -0,0 +1,26 @@
+
+/* globals gauge*/
+"use strict";
+const { openBrowser,write, closeBrowser, goto, press,text, contains, focus, into, textField, $, click} = require('taiko');
+const assert = require("assert");
+
+//const headless = process.env.headless_chrome.toLowerCase() == 'true' ? true : false;
+//beforeSuite(async () => openBrowser({ headless: headless }));
+//afterSuite(async () => closeBrowser());
+//step("On the Login page", async () => goto('http://newtours.demoaut.com/mercurysignon.php'));
+step("On the Login page", async () => goto(process.env.app_endpoint+'/mercurysignon.php'));
+
+step("Login as <username> using <password>", async (username, password) => {
+	//await write(username, into('User Name:')));
+	//await write(password, into('Password:'));
+
+	/* Using Xpaths and CSS since AUT does not have correct label tags maintained */
+	await write(username, into($("//input[@name='userName']")));
+	await write(password, into($("//input[@name='password']")));
+	await click(into($("//input[@name='login']")));
+
+})
+
+step("Page contains <content>", async (content) => {
+    assert.equal('Exists', (await text(contains(content)).exists()).description);
+});
